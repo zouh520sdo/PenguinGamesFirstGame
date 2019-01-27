1fb452c8b7748d71b5ce99e38bbec47fc488667e
Huang Zou <zouh520sdo@gmail.com>
2019-01-27 17:01:54 -0500

    Added simple visual effect for animing wandable object

--- Assets/Scripts/Wandable.cs
+++ Assets/Scripts/Wandable.cs
@@ -6,10 +6,12 @@ using UnityEngine;
 public class Wandable : MonoBehaviour {
 
     private Renderer _renderer;
+    private float _animingTime;
 
 	// Use this for initialization
 	void Start () {
         _renderer = GetComponent<Renderer>();
+        _animingTime = 0;
     }
 	
 	// Update is called once per frame
@@ -19,11 +21,13 @@ public class Wandable : MonoBehaviour {
 
     public void OnAniming ()
     {
-        _renderer.material.color = Color.yellow;
+        _animingTime += Time.deltaTime;
+        _renderer.material.color = Color.Lerp(Color.white, Color.yellow, Mathf.Sin(2 * Mathf.PI * _animingTime));
     }
 
     public void OffAniming ()
     {
         _renderer.material.color = Color.white;
+        _animingTime = 0;
     }
 }
