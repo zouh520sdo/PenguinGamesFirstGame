1692c1c020393811c8bd31aedffeadc5ac0df3fa
Huang Zou <zouh520sdo@gmail.com>
2019-01-30 16:19:58 -0500

    Added absorb and release function for wand

--- Assets/Scripts/Wand.cs
+++ Assets/Scripts/Wand.cs
@@ -5,32 +5,74 @@ using UnityEngine;
 public class Wand : MonoBehaviour {
 
     public float heat;
+    public float minHeat;
+    public float maxHeat;
     public float wandRange;
     public Wandable wandable;
+    public float absorbRatio;
+    public float releaseRatio;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, wandRange, LayerMask.NameToLayer("Wandable")))
+
+        if (Input.GetKey(KeyCode.E))
         {
-            print("Hit wandable.");
-            print(hit.collider.name);
-            wandable = hit.collider.gameObject.GetComponent<Wandable>();
-            wandable.OnAniming();
-        }
-        else
-        {
-            if (wandable != null)
+            Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, wandRange, LayerMask.NameToLayer("Wandable")))
             {
-                wandable.OffAniming();
-                wandable = null;
+                wandable = hit.collider.gameObject.GetComponent<Wandable>();
+                wandable.OnAiming();
             }
+            else
+            {
+                if (wandable != null)
+                {
+                    wandable.OffAiming();
+                    wandable = null;
+                }
+            }
+
+            // 
+            if (Input.GetButton("Fire1"))
+            {
+                // Absorb
+                print("Absorb");
+                absorbFrom(wandable);
+            }
+
+            if (Input.GetButton("Fire2"))
+            {
+                // Release
+                print("Release");
+                releaseTo(wandable);
+            }
+
+        }
+    }
+
+    /// <summary>
+    /// Absorb heat from the given wandable object if it is not null
+    /// </summary>
+    /// <param name="w">Given wandable object</param>
+    public void absorbFrom(Wandable w)
+    {
+        if (w)
+        {
+            heat = Mathf.Min(maxHeat, heat + w.heatLose(absorbRatio * Time.deltaTime, maxHeat - heat));
+        }
+    }
+
+    public void releaseTo(Wandable w)
+    {
+        if (w)
+        {
+            heat = Mathf.Max(minHeat, heat - w.heatGain(releaseRatio * Time.deltaTime, heat - minHeat));
         }
     }
 }
--- Assets/Scripts/Wandable.cs
+++ Assets/Scripts/Wandable.cs
@@ -8,9 +8,10 @@ public class Wandable : MonoBehaviour {
     public float maxHeat;
     public float minHeat;
     public float containingHeat;
+    public float volume;
 
-    private Renderer _renderer;
-    private float _animingTime;
+    protected Renderer _renderer;
+    protected float _animingTime;
 
 	// Use this for initialization
 	void Start () {
@@ -23,16 +24,44 @@ public class Wandable : MonoBehaviour {
 		
 	}
 
-    public void OnAniming ()
+    public void OnAiming ()
     {
         _animingTime += Time.deltaTime;
         Color targetColor = Color.Lerp(Color.cyan, Color.yellow, (containingHeat-minHeat) / (maxHeat - minHeat));
         _renderer.material.color = Color.Lerp(Color.white, targetColor, Mathf.Sin(2 * Mathf.PI * _animingTime));
     }
 
-    public void OffAniming ()
+    public void OffAiming ()
     {
         _renderer.material.color = Color.white;
         _animingTime = 0;
+    }
+
+    /// <summary>
+    /// Attemp to decrease heat in this object
+    /// </summary>
+    /// <param name="amount">amount to decrease</param>
+    /// <param name="wandRemaining">amount remaining space in wand to be empty</param>
+    /// <returns>The actual decreasing amount </returns>
+    public float heatLose(float amount, float wandRemaining)
+    {
+        float temp = Mathf.Max(minHeat, containingHeat - amount);
+        float diff = Mathf.Min(containingHeat - temp, wandRemaining);
+        containingHeat -= diff;
+        return diff;
+    }
+
+    /// <summary>
+    /// Attemp to increase heat in this object
+    /// </summary>
+    /// <param name="amount">amount to increase</param>
+    /// <param name="wandRemaining">amount remaining space in wand to be full</param>
+    /// <returns>The actual increasing amount</returns>
+    public float heatGain(float amount, float wandRemaining)
+    {
+        float temp = Mathf.Min(maxHeat, containingHeat + amount);
+        float diff = Mathf.Min(temp - containingHeat, wandRemaining);
+        containingHeat += diff;
+        return diff;
     }
 }
