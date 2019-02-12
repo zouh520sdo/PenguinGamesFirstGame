975ebc6f2e6db0a0f2991025787364dcebec33eb
Huang Zou <zouh520sdo@gmail.com>
2019-02-11 19:21:15 -0500

    Added screen fade in/out effect when activate crystal

--- Assets/Scripts/Crystal.cs
+++ Assets/Scripts/Crystal.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Crystal : Pickable {
 
+    public Image blackScreen;
     public GameManager gameManager;
     public List<ObjectIdentifier> resettingObjects;
+
+    public float fadeOutDuration;
+    public float fadeInDuration;
 
     public override void Start()
     {
@@ -21,8 +26,9 @@ public class Crystal : Pickable {
         base.Pick(p);
         print("Hit " + name);
         Drop(p);
-        ResetObjects(); // Reset related objects in corresponding area
-        gameManager.SaveGame();  // Save games
+
+        // Start to black out, save, and then fade in to game
+        StartCoroutine(FadeOutToBlack());
     }
 
     public override void Drop(Wand p)
@@ -37,5 +43,38 @@ public class Crystal : Pickable {
         {
             ob.SendMessage("OnReset", SendMessageOptions.DontRequireReceiver);
         }
+    }
+
+    IEnumerator FadeOutToBlack()
+    {
+        float timestamp = Time.time;
+        Color tempColor = blackScreen.color;
+        while (Time.time - timestamp < fadeOutDuration)
+        {
+            tempColor.a = (Time.time - timestamp) / fadeOutDuration;
+            blackScreen.color = tempColor;
+            yield return null;
+        }
+        tempColor.a = 1;
+        blackScreen.color = tempColor;
+
+        ResetObjects(); // Reset related objects in corresponding area
+        gameManager.SaveGame();  // Save games
+
+        StartCoroutine(FadeInToGame());
+    }
+
+    IEnumerator FadeInToGame()
+    {
+        float timestamp = Time.time;
+        Color tempColor = blackScreen.color;
+        while (Time.time - timestamp < fadeInDuration)
+        {
+            tempColor.a = 1 -  (Time.time - timestamp) / fadeInDuration;
+            blackScreen.color = tempColor;
+            yield return null;
+        }
+        tempColor.a = 0;
+        blackScreen.color = tempColor;
     }
 }
