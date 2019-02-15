8790cbeae2c2220250c634574b6e21ef2afa2a11
Huang Zou <zouh520sdo@gmail.com>
2019-02-15 16:14:29 -0500

    Enabled color changing for MatrixTrigger

--- Assets/Scripts/MatrixTrigger.cs
+++ Assets/Scripts/MatrixTrigger.cs
@@ -5,12 +5,40 @@ using UnityEngine;
 public class MatrixTrigger : Trigger {
 
     // May need to override activateTriggees and deactivateTriggees
+    public Material blueMaterial;
+    public Material redMaterial;
+
+    protected Material originalMat;
+
+    public override void Start()
+    {
+        base.Start();
+        originalMat = GetComponent<Renderer>().material;
+    }
+
+    public override void OnReset()
+    {
+        base.OnReset();
+        GetComponent<Renderer>().material = originalMat;
+    }
+
+    public override void activateTriggees()
+    {
+        base.activateTriggees();
+        GetComponent<Renderer>().material = redMaterial;
+    }
+
+    public override void deactivateTriggees()
+    {
+        base.deactivateTriggees();
+        GetComponent<Renderer>().material = blueMaterial;
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
         // activate
         if (collision.collider.GetComponent<RedBlock>())
-        {
+        { 
             activateTriggees();
         }
     }
