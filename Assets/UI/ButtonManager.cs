3abc2924b361569e25b3da79d65a37c7b33c8217
Eve <yueqing_dai@yahoo.com>
2019-02-19 18:51:02 -0500

    start scene function pause function

---
+++ Assets/UI/ButtonManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+
+    public GameObject SettingPanel;
+
+	public void NewGameBtn(string newGameLevel)
+    {
+        SceneManager.LoadScene(newGameLevel);
+    }
+
+    public void ExitGameBtn()
+    {
+        Application.Quit();
+    }
+
+    public void SettingBtn()
+    {
+        SettingPanel.SetActive(true);
+    }
+
+    public void BackBtn()
+    {
+        SettingPanel.SetActive(false);
+    }
+}
---
+++ Assets/UI/PauseMenu.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+    [SerializeField] private GameObject PauseMenuUI;
+
+    [SerializeField] private bool isPaused;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+        }
+
+        if (isPaused)
+        {
+            ActivateMenu();
+        }
+
+        else
+        {
+            DeactivateMenu();
+        }
+	}
+
+    void ActivateMenu()
+    {
+        Time.timeScale = 0;
+        AudioListener.pause = true;
+        PauseMenuUI.SetActive(true);
+    }
+
+    public void DeactivateMenu()
+    {
+        Time.timeScale = 1;
+        AudioListener.pause = false;
+        PauseMenuUI.SetActive(false);
+        isPaused = false;
+    }
+}
