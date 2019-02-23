01459895bdecfc42b22bbde18a96c4ce0043a8dc
Huang Zou <zouh520sdo@gmail.com>
2019-02-23 18:27:42 -0500

    Added all books to reset list

---
+++ Assets/Scripts/BooksGroup.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BooksGroup : MonoBehaviour {
+
+    ObjectIdentifier[] children;
+
+    void Start()
+    {
+        children = gameObject.GetComponentsInChildren<ObjectIdentifier>();
+    }
+
+    public void OnReset()
+    {
+        foreach (ObjectIdentifier child in children)
+        {
+            Ignitable book = child.GetComponent<Ignitable>();
+            if (book)
+            {
+                book.OnReset();
+            }
+        }
+    }
+}
