cbb7186b262616d5dbe9470386e7630236c1803f
Eve <yueqing_dai@yahoo.com>
2019-02-25 17:11:20 -0500

    book ignite sound

--- Assets/Scripts/BooksGroup.cs
+++ Assets/Scripts/BooksGroup.cs
@@ -6,9 +6,27 @@ public class BooksGroup : MonoBehaviour {
 
     ObjectIdentifier[] children;
 
+    public AudioSource OnFire;
+
     void Start()
     {
         children = gameObject.GetComponentsInChildren<ObjectIdentifier>();
+        OnFire = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        foreach (ObjectIdentifier child in children)
+        {
+            Ignitable book = child.GetComponent<Ignitable>();
+            if (book && book.isOnFire)
+            {
+                if(!OnFire.isPlaying)
+                {
+                    OnFire.Play();
+                }
+            }
+        }
     }
 
     public void OnReset()
