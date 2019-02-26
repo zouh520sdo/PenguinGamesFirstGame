327d9914ce1a2496ff3bac82d115d722f1dbc070
Eve <yueqing_dai@yahoo.com>
2019-02-25 21:39:48 -0500

    Implement magic sound

---
+++ Assets/Scripts/PlayMagic.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayMagic : MonoBehaviour {
+
+    public AudioSource Magic;
+
+    public Image Marker;
+    public Sprite Aing;
+    public Sprite Ring;
+
+    // Use this for initialization
+    void Start()
+    {
+        Magic = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Marker.sprite == Aing || Marker.sprite == Ring)
+        {
+            if (!Magic.isPlaying)
+            {
+                Magic.Play();
+            }
+        }
+        else
+        {
+            //Debug.Log("false");
+            Magic.Stop();
+        }
+
+    }
+}
