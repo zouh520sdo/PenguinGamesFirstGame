3e66a5c2ea2ef6f40986767959df6876fd191a3e
Huang Zou <zouh520sdo@gmail.com>
2019-02-26 16:56:28 -0500

    Enabled activiation and deactivation for magic wall

---
+++ Assets/Scripts/BlueWallT.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueWallT : Triggee
+{
+    public List<Renderer> allRenderers;
+    public List<Collider> allColliders;
+
+    public virtual void SetActive(bool active)
+    {
+        isActive = active;
+        foreach (Renderer r in allRenderers)
+        {
+            r.enabled = active;
+            Color tempColor = r.material.color;
+            if (active)
+            {
+                tempColor.a = 1f;
+                r.material.color = tempColor;
+            }
+            else
+            {
+                tempColor.a = 0f;
+                r.material.color = tempColor;
+            }
+        }
+
+        foreach (Collider c in allColliders)
+        {
+            c.enabled = active;
+        }
+    }
+
+    public override void Start()
+    {
+        base.Start();
+        allRenderers = new List<Renderer>(gameObject.GetComponentsInChildren<Renderer>());
+        allColliders = new List<Collider>(gameObject.GetComponentsInChildren<Collider>());
+
+        SetActive(isActive);
+    }
+
+    public override void Activate()
+    {
+        base.Activate();
+        StartCoroutine(FadeIn());
+    }
+
+    public override void Deactivate()
+    {
+        base.Deactivate();
+        StartCoroutine(FadeOut());
+    }
+
+    IEnumerator FadeIn()
+    {
+        float alpha = 0f;
+        foreach (Renderer r in allRenderers)
+        {
+            r.enabled = true;
+        }
+        while (alpha <= 1f)
+        {
+            foreach (Renderer r in allRenderers)
+            {
+                Color tempColor = r.material.color;
+                tempColor.a = alpha;
+                r.material.color = tempColor;
+            }
+
+            alpha += Time.deltaTime;
+            yield return null;
+        }
+        SetActive(true);
+    }
+
+    IEnumerator FadeOut()
+    {
+        float alpha = 1f;
+        while (alpha >= 0f)
+        {
+            foreach (Renderer r in allRenderers)
+            {
+                Color tempColor = r.material.color;
+                tempColor.a = alpha;
+                r.material.color = tempColor;
+            }
+
+            alpha -= Time.deltaTime;
+            yield return null;
+        }
+        SetActive(false);
+    }
+}
---
+++ Assets/Scripts/DoorInDoor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorInDoor : MonoBehaviour {
+
+    public bool isOpen;
+    public Ignitable giantBook;
+
+    protected Animator animator;
+    protected bool originalIsOpen;
+
+    public void OnReset()
+    {
+        isOpen = originalIsOpen;
+    }
+
+	// Use this for initialization
+	void Start () {
+        originalIsOpen = isOpen;
+        animator = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (giantBook)
+        {
+            isOpen = !giantBook.invisibleData.isVisible;
+        }
+        animator.SetBool("IsOpen", isOpen);
+    }
+}
