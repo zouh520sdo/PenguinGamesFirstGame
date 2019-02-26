97a84ad327ef2facfa7477be00fa6b16bd59c353
Eve <yueqing_dai@yahoo.com>
2019-02-25 19:30:25 -0500

    GiantBook fire sound

---
+++ Assets/Scripts/PlayFireSound.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayFireSound : MonoBehaviour {
+
+    public GameObject CanFire;
+    public AudioSource OnFire;
+    private bool isOnfire;
+
+    // Use this for initialization
+    void Start () {
+        OnFire = GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        isOnfire = CanFire.GetComponent<Ignitable>().isOnFire;
+        if (isOnfire)
+        {
+            if (!OnFire.isPlaying)
+            {
+                OnFire.Play();
+            }
+        }
+        else
+        {
+            OnFire.Stop();
+        }
+		
+	}
+}
