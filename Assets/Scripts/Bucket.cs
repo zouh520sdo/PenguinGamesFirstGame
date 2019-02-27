6394dc6c652f039fcef886b1c2d280948c2fd9f5
Huang Zou <zouh520sdo@gmail.com>
2019-02-27 18:23:55 -0500

    Tinkered on floating bucket

---
+++ Assets/Scripts/Bucket.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bucket : Pickable {
+
+    public float floatingHeight;
+
+    public override void Update()
+    {
+        base.Update();
+
+        if (!isPickedUp)
+        {
+            Vector3 targetPos = transform.position;
+            targetPos.y = floatingHeight;
+
+            targetPos.y += Mathf.Sin(2f * Time.time);
+
+            transform.position = Vector3.Lerp(transform.position, targetPos, 1.5f * Time.deltaTime);
+        }
+    }
+}
