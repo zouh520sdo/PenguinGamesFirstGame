feb576e076b076d66e6cb5043e73345c78006f96
Huang Zou <zouh520sdo@gmail.com>
2019-03-20 01:54:34 -0400

    Added scripts for Jigsaw pieces

---
+++ Assets/Scripts/JigsawPeice.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum JigsawType {
+    triangle,
+    square
+}
+
+public class JigsawPeice : MonoBehaviour {
+
+    public JigsawType type;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
