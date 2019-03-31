b9af4f01f1fb384d6217c054deca6514d007da2d
Huang Zou <zouh520sdo@gmail.com>
2019-03-30 22:54:24 -0400

    Added crystal to level 4; and enabled reseting for platforms

---
+++ Assets/Scripts/Platforms.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platforms : MonoBehaviour {
+
+    public void OnReset()
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SendMessage("OnReset", SendMessageOptions.DontRequireReceiver);
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
