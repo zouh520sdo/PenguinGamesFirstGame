f9f783d24e26acb6125252ad5bfaa7977643baed
Huang Zou <zouh520sdo@gmail.com>
2019-03-30 22:17:40 -0400

    Added risable script to certain platforms and made certain platforms triggers

---
+++ Assets/Scripts/RisablePlatform.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RisablePlatform : Wandable {
+
+    public float lowHeight;
+    public float highHeight;
+
+    protected override void OnStart()
+    {
+        base.OnStart();
+
+        float height = transform.position.y;
+        containingHeat = (height - lowHeight) / (highHeight - lowHeight) * (maxHeat - minHeat) + minHeat;
+        containingHeat = Mathf.Min(maxHeat, Mathf.Max(minHeat, containingHeat));
+    }
+
+    protected override void OnUpdate()
+    {
+        base.OnUpdate();
+
+        Vector3 tempPos = transform.position;
+        tempPos.y = ((containingHeat - minHeat) / (maxHeat - minHeat)) * (highHeight - lowHeight) + lowHeight;
+        transform.position = tempPos;
+    }
+}
