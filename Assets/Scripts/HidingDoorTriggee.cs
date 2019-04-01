438fdea9e07aac5269de2038e168e353db9dc1bc
Huang Zou <zouh520sdo@gmail.com>
2019-03-31 22:56:36 -0400

    Added triggee for showing door and open door

---
+++ Assets/Scripts/HidingDoorTriggee.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HidingDoorTriggee : Triggee {
+
+    public override void Activate()
+    {
+        base.Activate();
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(true);
+        }
+    }
+
+    public override void Deactivate()
+    {
+        base.Deactivate();
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+    }
+}
