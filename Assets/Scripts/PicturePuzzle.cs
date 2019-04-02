b970fbb99341f420291658191aa15f4090443584
Huang Zou <zouh520sdo@gmail.com>
2019-04-02 18:45:26 -0400

    Added picture puzzles in level 5; set up triggers in picture puzzles; haven't tested

---
+++ Assets/Scripts/PicturePuzzle.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Answer
+{
+    Red,
+    Blue
+}
+
+public class PicturePuzzle : MonoBehaviour {
+
+    public List<PuzzlePicture> pictures;
+    public Answer correctAnswer;
+
+    protected bool originalIsActive;
+
+    public void Start()
+    {
+        pictures.AddRange(GetComponentsInChildren<PuzzlePicture>());
+        InitializePuzzle();
+    }
+
+    public void OnReset()
+    {
+        InitializePuzzle();
+    }
+
+    public void Update()
+    {
+
+    }
+
+    public void InitializePuzzle ()
+    {
+        if (pictures.Count >= 2)
+        {
+            int haha = Random.Range(1, 7);
+
+            switch (haha)
+            {
+                // All red
+                case 1:
+                    pictures[0].isRed = true;
+                    pictures[1].isRed = true;
+                    correctAnswer = Answer.Red;
+                    break;
+                // you blue, other red
+                case 2:
+                    pictures[0].isRed = true;
+                    pictures[1].isRed = true;
+                    correctAnswer = Answer.Blue;
+                    break;
+                // You red, one red, and one blue
+                case 3:
+                    pictures[0].isRed = true;
+                    pictures[1].isRed = false;
+                    correctAnswer = Answer.Red;
+                    break;
+                // You red, one blue, and one red
+                case 4:
+                    pictures[0].isRed = false;
+                    pictures[1].isRed = true;
+                    correctAnswer = Answer.Red;
+                    break;
+                // You Blue, one red, and one blue
+                case 5:
+                    pictures[0].isRed = true;
+                    pictures[1].isRed = false;
+                    correctAnswer = Answer.Blue;
+                    break;
+                // You blue, one blue, and one red
+                case 6:
+                    pictures[0].isRed = false;
+                    pictures[1].isRed = true;
+                    correctAnswer = Answer.Blue;
+                    break;
+
+                // !All blue
+                case 7:
+                    pictures[0].isRed = false;
+                    pictures[1].isRed = false;
+                    correctAnswer = Answer.Blue;
+                    break;
+
+                // !You red, other blue
+                case 8:
+                    pictures[0].isRed = false;
+                    pictures[1].isRed = false;
+                    correctAnswer = Answer.Red;
+                    break;
+            }
+
+            pictures[0].isHandsUp = (pictures[1].isRed && correctAnswer == Answer.Red);
+            pictures[1].isHandsUp = (pictures[0].isRed && correctAnswer == Answer.Red);
+        }
+    }
+}
---
+++ Assets/Scripts/PicturePuzzleTrigger.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PicturePuzzleTrigger : Trigger {
+
+    public PicturePuzzle puzzle;
+    public Crystal crystal;
+
+    protected PuzzleSelectionBlock block;
+
+    public override void OnReset()
+    {
+        base.OnReset();
+        if (puzzle)
+        {
+            puzzle.SendMessage("OnReset");
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!block)
+        {
+            PuzzleSelectionBlock b = collision.collider.GetComponent<PuzzleSelectionBlock>();
+            if (b && puzzle)
+            {
+                if (b.answer == puzzle.correctAnswer)
+                {
+                    activateTriggees();
+                }
+                else
+                {
+                    crystal.Pick(GameObject.FindWithTag("Player").GetComponent<Wand>());
+                }
+            }
+        }
+    }
+}
