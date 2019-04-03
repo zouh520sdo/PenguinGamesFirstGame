2a516d4c5cfbc31dea087a547dee7418be8a682e
Huang Zou <zouh520sdo@gmail.com>
2019-04-03 02:31:24 -0400

    Rearranged the hierarchy in level 5; set up resetting objects

--- Assets/Scripts/PuzzleSelectionBlock.cs
+++ Assets/Scripts/PuzzleSelectionBlock.cs
@@ -6,5 +6,22 @@ public class PuzzleSelectionBlock : MonoBehaviour {
 
     public Answer answer;
 
+    protected Answer originalAnswer;
+    protected Vector3 originalPos;
+    protected Quaternion originalRot;
+
+    private void Start()
+    {
+        originalAnswer = answer;
+        originalPos = transform.position;
+        originalRot = transform.rotation;
+    }
+
+    public void OnReset()
+    {
+        answer = originalAnswer;
+        transform.position = originalPos;
+        transform.rotation = originalRot;
+    }
 
 }
