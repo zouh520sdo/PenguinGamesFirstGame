f6d29179e685151d5b2b115cf8f39d9ab457a4c0
Huang Zou <zouh520sdo@gmail.com>
2019-04-10 21:28:09 -0400

    Added some voice overs in level 1 and level 2

--- Assets/Scripts/OpeningEveNote.cs
+++ Assets/Scripts/OpeningEveNote.cs
@@ -4,5 +4,47 @@ using UnityEngine;
 
 public class OpeningEveNote : Note {
 
+    public AudioSource audioSource;
+    public List<AudioClip> voiceOvers;
 
+    public override void Start()
+    {
+        base.Start();
+        if (!audioSource)
+        {
+            audioSource = GetComponent<AudioSource>();
+        }
+    }
+
+    public override string nextLine()
+    {
+        if (paragraphIndex >= paragraph.Count - 1)
+        {
+            paragraphIndex = -1;
+            finished = true;
+            if (audioSource)
+            {
+                audioSource.Stop();
+                audioSource.clip = null;
+            }
+            return "";
+        }
+        else
+        {
+            paragraphIndex++;
+            if (audioSource)
+            {
+                audioSource.Stop();
+                if (voiceOvers.Count > paragraphIndex)
+                {
+                    audioSource.clip = voiceOvers[paragraphIndex];
+                    if (audioSource.clip)
+                    {
+                        audioSource.Play();
+                    }
+                }
+            }
+            return paragraph[paragraphIndex];
+        }
+    }
 }
