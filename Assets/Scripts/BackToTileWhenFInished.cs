e9d0d023607472c1b741573e1f096de559065259
Huang Zou <zouh520sdo@gmail.com>
2019-04-17 14:38:22 -0400

    Temp stable

---
+++ Assets/Scripts/BackToTileWhenFInished.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackToTileWhenFInished : MonoBehaviour {
+
+    public bool isActive;
+    public Note note;
+    public int sceneIndex;
+
+	// Use this for initialization
+	void Start () {
+		if (!note)
+        {
+            note = GetComponent<Note>();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isActive)
+        {
+            if (note.getIsFinished())
+            {
+                isActive = true;
+                SceneManager.LoadScene(sceneIndex);
+            }
+        }
+	}
+}
