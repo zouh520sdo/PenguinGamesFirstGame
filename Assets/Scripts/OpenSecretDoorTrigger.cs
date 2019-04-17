50150fbebf4d10f3d1377fcdb07bd524b15bad9a
Huang Zou <zouh520sdo@gmail.com>
2019-04-17 14:21:50 -0400

    Enabled entering secret ending

--- Assets/Scripts/OpenSecretDoorTrigger.cs
+++ Assets/Scripts/OpenSecretDoorTrigger.cs
@@ -1,16 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class OpenSecretDoorTrigger : MonoBehaviour {
 
+    public Note secretNote;
+    public bool isActive;
+
 	// Use this for initialization
 	void Start () {
-		
+		if (!secretNote)
+        {
+            secretNote = GetComponent<Note>();
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (!isActive)
+        {
+            if (secretNote.finished)
+            {
+                isActive = true;
+                SceneManager.LoadScene(6);
+            }
+        }
 	}
 }
