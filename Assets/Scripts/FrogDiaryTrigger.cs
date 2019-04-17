bda7515370bd0fffa8db31bc58b72f7d0fe4a989
Huang Zou <zouh520sdo@gmail.com>
2019-04-17 17:51:43 -0400

    Added frog's dialogue in level 4

---
+++ Assets/Scripts/FrogDiaryTrigger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrogDiaryTrigger : MonoBehaviour {
+
+    public bool isActive;
+    public Diary diary;
+    public GameManager gameManager;
+    public Note note;
+
+
+	// Use this for initialization
+	void Start () {
+		if (!gameManager)
+        {
+            gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+        }
+        if (!note)
+        {
+            note = GetComponent<Note>();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isActive)
+        {
+            int hasGua = gameManager.GetHasDua();
+            if (hasGua != 0 && diary.getIsFinished()) 
+            {
+                isActive = true;
+                gameManager.StartNote(note);
+            }
+        }
+	}
+}
