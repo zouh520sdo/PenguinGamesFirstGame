572272161b63b6f4c373eb860f0df00d54410942
Huang Zou <zouh520sdo@gmail.com>
2019-04-20 02:23:28 -0400

    Created lamp wandable to secret lamp

---
+++ Assets/Scripts/LampWandable.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LampWandable : Wandable {
+
+    public Light myLight;
+    public bool isOnLight;
+    protected bool originalIsOnLight;
+    protected float lightOnIntensity;
+    protected float lightOffIntensity;
+
+
+    protected override void OnStart()
+    {
+        base.OnStart();
+        lightOnIntensity = 2.71f;
+        lightOffIntensity = 0f;
+        originalIsOnLight = isOnLight;
+    }
+
+    public override void OnReset()
+    {
+        base.OnReset();
+        isOnLight = originalIsOnLight;
+    }
+
+    protected override void OnUpdate()
+    {
+        base.OnUpdate();
+        myLight.intensity = (lightOnIntensity - lightOffIntensity) * ((containingHeat - minHeat) / (maxHeat - minHeat)) + lightOffIntensity;
+
+        if (containingHeat >= maxHeat)
+        {
+            isOnLight = true;
+        }
+        else
+        {
+            isOnLight = false;
+        }
+
+        if (isOnLight)
+        {
+            myLight.intensity = lightOnIntensity;
+        }
+    }
+}
---
+++ Assets/Scripts/SecretDiaryLampTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecretDiaryLampTrigger : Trigger {
+
+
+    public GameObject targetObject;
+
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+    }
+}
