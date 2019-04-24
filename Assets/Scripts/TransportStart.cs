30fb78e2dc0dee05b43354be70b41a3eb945fa26
Francys Dong <francysdong@gmail.com>
2019-04-23 21:24:35 -0400

    Transportation circles at start of scene (only first lvl now for test

---
+++ Assets/Scripts/TransportStart.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransportStart : MonoBehaviour {
+
+    private Animator platform;
+    public GameObject effect;
+
+
+	// Use this for initialization
+	void Start () {
+		platform = this.GetComponentInChildren<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.tag == "Player")
+        {
+            effect.SetActive(false);
+            platform.SetTrigger("end");
+            
+        }
+        
+    }
+
+
+}
