6339d4e32df1727d130c6bbd06ea369b6b7c538e
Huang Zou <zouh520sdo@gmail.com>
2019-05-16 16:55:50 -0400

    Created SpecialThanksManager

---
+++ Assets/Scripts/SpecialThanksManger.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpecialThanksManger : MonoBehaviour {
+
+    //           -3.8
+    //16.7 1.23 -52.7
+    public List<Vector3> cameraPoses;
+    public float movingRatio;
+    public Camera myCamera;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator SlidingToPos(Vector3 targetPos)
+    {
+        while (Vector3.Distance(targetPos, myCamera.transform.position) > 0.02f)
+        {
+            myCamera.transform.position = Vector3.Lerp(myCamera.transform.position, targetPos, movingRatio * Time.deltaTime);
+            yield return null;
+        }
+
+        myCamera.transform.position = targetPos;
+    }
+
+    public void MoveToPosIndex(int index)
+    {
+        if (index >= 0 && index < cameraPoses.Count)
+        {
+            StartCoroutine(SlidingToPos(cameraPoses[index]));
+        }
+    }
+}
