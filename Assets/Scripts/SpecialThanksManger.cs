1b1841cdd2d2645fc7c5e0e02ae3b4878241cc04
Huang Zou <zouh520sdo@gmail.com>
2019-05-16 22:52:55 -0400

    Added special thanks in credits

--- Assets/Scripts/SpecialThanksManger.cs
+++ Assets/Scripts/SpecialThanksManger.cs
@@ -10,14 +10,27 @@ public class SpecialThanksManger : MonoBehaviour {
     public float movingRatio;
     public Camera myCamera;
 
+    public GameObject backInCredits;
+    public GameObject backInSpecialThanks;
+    public GameObject nextButton;
+
+    protected IEnumerator movingCoroutine;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        SwitchToBackInCredits();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            MoveToPosIndex(0);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            MoveToPosIndex(1);
+        }
 	}
 
     IEnumerator SlidingToPos(Vector3 targetPos)
@@ -35,7 +48,28 @@ public class SpecialThanksManger : MonoBehaviour {
     {
         if (index >= 0 && index < cameraPoses.Count)
         {
-            StartCoroutine(SlidingToPos(cameraPoses[index]));
+            if (movingCoroutine != null)
+            {
+                StopCoroutine(movingCoroutine);
+                movingCoroutine = null;
+            }
+            print(cameraPoses[index]);
+            movingCoroutine = SlidingToPos(cameraPoses[index]);
+            StartCoroutine(movingCoroutine);
         }
+    }
+
+    public void SwitchToBackInCredits()
+    {
+        backInCredits.SetActive(true);
+        backInSpecialThanks.SetActive(false);
+        nextButton.SetActive(true);
+    }
+
+    public void SwitchToBackInSpecialThanks()
+    {
+        backInCredits.SetActive(false);
+        backInSpecialThanks.SetActive(true);
+        nextButton.SetActive(false);
     }
 }
